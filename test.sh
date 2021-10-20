4dc924e38fb04802572151906b0cb1b4cee8137a
Brandon Bloom <brandon@brandonbloom.name>
2021-10-20 16:34:44 -0700

    make test.sh actually fail if a broken link is found

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 set -e
 
 # TODO: Something dramatically more efficient than this...
-find . -name '*.md' \
-  -exec docker run \
-    -v $PWD:/tmp:ro \
-    --rm -i \
-    --workdir /tmp \
-    ghcr.io/tcort/markdown-link-check:stable \
-    --quiet \
-    {} \;
+find . -name '*.md' |
+  xargs -n1 \
+    docker run \
+      -v $PWD:/tmp:ro \
+      --rm -i \
+      --workdir /tmp \
+      ghcr.io/tcort/markdown-link-check:stable \
+      --quiet
